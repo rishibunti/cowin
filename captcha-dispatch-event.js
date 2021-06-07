9c7404fe2293b3d104c6b990c5b230c923cb9c82
rishibunti <64738563+rishibunti@users.noreply.github.com>
2021-06-07 23:27:45 +0530

    Update captcha-dispatch-event.js

--- captcha-dispatch-event.js
+++ captcha-dispatch-event.js
@@ -22,10 +22,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 $(document).on('click', 'div.slots-box:not(.no-available)', async function (e) {
-    await sleep(1000);
+    await sleep(300);
     waitForEl('.appoint-success', () => {
-
+        try {
+            enterCaptcha();
+        } catch (e) {
+            console.log("Unable to enter captcha");
+        } finally {
+            if (enableautoconfirm) {
                 $("ion-button.confirm-btn")[0].click();
-       
+            }
+        }
     });
 });
